0e085cbb4abb1b27846fcdf9d948df55c3db6738
24lizikri <muhammadlizikriarromadhon@gmail.com>
2024-07-04 08:44:35 +0700

    welcome blade

---
+++ resources/views/layanan.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layanan | DINAS PENDIDIKAN DAN KEBUDAYAAN KOTA MALANG</title>
+</head>
+<body>
+    HELLO WORD
+</body>
+</html>
