0915beabc103e0e8b78da56a0bc1c89513d04385
addindarizki <adindakartikasari91@gmail.com>
2024-07-05 13:24:03 +0700

    tambah fitur

--- resources/views/home/pages/home/welcome.blade.php
+++ resources/views/home/pages/home/welcome.blade.php
@@ -42,14 +42,14 @@
       <nav id="navmenu" class="navmenu">
         <ul>
           <li><a href="index.html#hero" class="active">Beranda</a></li>
-          <li><a href="{{ route('home.layanan.index') }}">Layanan</a></li>
+          <li><a href="#featured-services">Layanan</a></li>
           <li><a href="{{ route('home.profil.index') }}">Profil</a></li>
           <li><a href="{{ route('home.berita.index') }}">Berita</a></li>
           <li><a href="{{ route('home.inovasi.index') }}">Inovasi</a></li>
           <li><a href="{{ route('home.galeri.index') }}">Galeri</a></li>
           <li><a href="{{ route('home.pembelajaran.index') }}">Pembelajaran</a></li>
           <li><a href="{{ route('home.informasi.index') }}">Informasi Publik</a></li>
-          <li><a href="{{ route('home.kontak.index') }}">Kontak</a></li>
+          <li><a href="#contact">Kontak</a></li>
           <!-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="#">Dropdown 1</a></li>
@@ -435,7 +435,7 @@
 
         <div class="row gy-4 mt-1">
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d688.1506512226823!2d112.61389434101307!3d-7.956754435114845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78827beffc2665%3A0x9dd38763e9a56b77!2sDinas%20Pendidikan%20dan%20Kebudayaan%20Kota%20Malang!5e0!3m2!1sen!2sid!4v1720151847698!5m2!1sen!2sid" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div><!-- End Google Maps -->
 
           <div class="col-lg-6">
