b5d6503c3b46b0e3b5cdcefbe9fb1d9eda44208c
zaidanarif73 <zaidanarif37@gmail.com>
2024-07-08 13:43:53 +0700

    berita fixed 2

--- resources/views/home/pages/home/welcome.blade.php
+++ resources/views/home/pages/home/welcome.blade.php
@@ -198,56 +198,31 @@
       </div>
 
     <!-- Features Details Section -->
-    <section id="features-details" class="features-details section">
-      <div class="container section-title" data-aos="fade-up">
-        <h2>Features</h2>
-      </div>
-      <div class="container">
+   <section id="features-details" class="features-details section">
+    <div class="container section-title" data-aos="fade-up">
+      <h2>Berita</h2>
+    </div>
+    <div class="container">
+      <h3>berita utama</h3>
 
-        <div class="row gy-4 justify-content-between features-item">
+      <div class="row gy-4 justify-content-between features-item">
 
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <img src="assets/img/features-1.jpg" class="img-fluid" alt="">
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+          <img src="assets/img/News.jpg" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+          <div class="content">
+            <p>
+              Jumat, 21 Juni 2024
+            </p>
+            <h4>Jadi Narsum Worhshop, Mas Kadin Ucapkan Terima Kasih pada PTK SMPN 12 Malang</h4>
+            <p>
+            Malang - Jajaran SMP Negeri 12 Malang yang kini dikepalai oleh M. Shodiq., M.Pd selenggarakan Workshop (WS) bertajuk Peningkatan Kompetensi Guru Berbasis Literasi dan Numerasi. Kegiatan yang diselengg...
+            </p>
+            <a href="#" class="btn more-btn">Read More</a>
           </div>
-
-          <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-            <div class="content">
-              <h3>Corporis temporibus maiores provident</h3>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
-              </p>
-              <a href="#" class="btn more-btn">Learn More</a>
-            </div>
-          </div>
-
-        </div><!-- Features Item -->
-
-        <!-- <div class="row gy-4 justify-content-between features-item">
-
-          <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="content">
-              <h3>Neque ipsum omnis sapiente quod quia dicta</h3>
-              <p>
-                Quidem qui dolore incidunt aut. In assumenda harum id iusto lorena plasico mares
-              </p>
-              <ul>
-                <li><i class="bi bi-easel flex-shrink-0"></i> Et corporis ea eveniet ducimus.</li>
-                <li><i class="bi bi-patch-check flex-shrink-0"></i> Exercitationem dolorem sapiente.</li>
-                <li><i class="bi bi-brightness-high flex-shrink-0"></i> Veniam quia modi magnam.</li>
-              </ul>
-              <p></p>
-              <a href="#" class="btn more-btn">Learn More</a>
-            </div>
-
-          </div>
-
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
-            <img src="assets/img/features-2.jpg" class="img-fluid" alt="">
-          </div>
-
-        </div>Features Item -->
+        </div>
 
       </div>
 
