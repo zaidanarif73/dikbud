8433ec3c43bf7dc97f7a29edf694a86ea5200d7d
zaidanarif73 <zaidanarif37@gmail.com>
2024-07-09 13:20:02 +0700

    login page template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,30 +5,39 @@
 
 <body>
     @include('sweetalert::alert')
-<section>
-    <form action="{{ route('auth.login.index') }}" method="POST" class="user">
-        @csrf
-        <div class="form-group">
-            <label>Email</label>
-            <input type="email" class="form-control p_input" name="email">
-        </div>
-        <div class="form-group">
-            <label>Password</label>
-            <input type="text" class="form-control p_input" name="password">
-        </div>
-        <div class="form-group d-flex align-items-center justify-content-between"">
-            <div class="form-check">
-                <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input"> Remember Me
-                </label>
+    <div class="d-lg-flex half">
+        <div class="bg order-1 order-md-2" style="background-image: url('{{ asset("assets/img/login/bg_1.jpg") }}');"></div>
+        <div class="contents order-2 order-md-1">
+    
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-7">
+                    <h3>Login to <strong>DIKBUUUUUUUUUUUUUUUUUUUUddd</strong></h3>
+                    <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+                            <form action="{{ route('auth.login.index') }}" method="POST" class="user">
+                                @csrf
+                                <div class="form-group first">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" placeholder="your-email@gmail.com" id="email" name="email">
+                                </div>
+                                <div class="form-group last mb-3">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" placeholder="Your Password" id="password" name="password">
+                                </div>
+                                <div class="d-flex mb-5 align-items-center">
+                                    <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
+                                        <input type="checkbox" checked="checked"/>
+                                        <div class="control__indicator"></div>
+                                    </label>
+                                    <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
+                                </div>
+                                <input type="submit" value="Log In" class="btn btn-block btn-primary">
+                            </form>
+                        </div>
+                </div>
             </div>
-            <a href="#" class="forgot-pass">Forgot Password</a>
         </div>
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
-        </div>
-    </form>
-</section>
+    </div>
 @include('home.layouts.script')
 </body>
 </html>
--- resources/views/home/layouts/script.blade.php
+++ resources/views/home/layouts/script.blade.php
@@ -11,6 +11,15 @@
 <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
 <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
 
+{{-- CDN --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 <!-- Main JS File -->
 <script src="assets/js/main.js"></script>
+
+{{-- login script --}}
+<script src="assets/js/login/jquery-3.3.1.min.js"></script>
+<script src="assets/js/login/popper.min.js"></script>
+<script src="assets/js/login/bootstrap.min.js"></script>
+<script src="assets/js/login/main.js"></script>
 @yield("script")
