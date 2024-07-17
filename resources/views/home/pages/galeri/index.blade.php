0d5419c9c12f6a9d5f22ac9f62cda4b16590f210
zaidanarif73 <zaidanarif37@gmail.com>
2024-07-17 16:04:23 +0700

    merge zikri-dinda-ucup

--- resources/views/home/pages/galeri/index.blade.php
+++ resources/views/home/pages/galeri/index.blade.php
@@ -12,13 +12,13 @@
             <div class="gallery">
                 
                 <div class="gallery-item">
-                    <a class="" data-bs-toggle="modal" href="#exampleModalToggle" alt="">
+                    <a class="" data-bs-toggle="modal" href="#userShowModal" alt="">
                         <img src="assets/img/galeri/galeri1.jpg" alt="">
                     </a>
                     <p>Senin, 4 Maret 2024 08:31 WIB</p>
                     <h3>Sosialisasi Kepangkatan dengan Kelompok Kerja Kepala Sekolah(K3S) SD se Kota Malang</h3>
                 </div>
-                <div class="gallery-item">
+                {{-- <div class="gallery-item">
                     <img src="assets/img/galeri/galeri2.jpg" alt="">
                     <p>Rabu, 21 Februari 2024 09:46 WIB</p>
                     <h3>Talkshow Pencegahan Pernikahan Dini dan Kesehatan Reproduksi Remaja</h3>
@@ -52,7 +52,7 @@
                     <img src="assets/img/galeri/galeri8.jpg" alt="">
                     <p>Senin, 13 Mei 2024 14:17 WIB</p>
                     <h3>Pentingnya Platform Merdeka Mengajar Guna Optimalisasi Kinerja Guru Dalam Mengajar</h3>
-            </div>
+                </div> --}}
         </main>
         <div class="pagination">
             <button>&laquo;</button>
@@ -168,31 +168,7 @@
             
         </div>
     </div> -->
-    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalToggleLabel">Detail</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <h1>Sosialisasi Kepangkatan dengan Kelompok Kerja Kepala Sekolah(K3S) SD se Kota Malang</h1>
-                    <img src="assets/img/galeri/galeri1.jpg" alt="">
-                    <br>
-                    <p>Senin, 4 Maret 2024 08:31 WIB</p>
-                    <h6>Bidang Ketenagaan Dinas Pendidikan dan Kebudayaan Kota Malang mengadakan Sosialisasi Kepangkatan dengan Kelompok Kerja Kepaka Sekolah(K3S) SD se Kota Malang. Sebelumnya, Bidang Ketenagaan sudah mengadakaan sosialisasi kepangkatan dengan guru melalui aplikasi Zoom. Dikarenakan pelaksanaan sosialisasi tersebut kurang maksimal dan efektif, maka diadakan sosialisasi secara tatap muka dengan K3S SD Se-Kota Malang ini. Kegiatan ini dilaksanakan pada tanggal 28 Februari 2024 yang bertempat di Ruang VIP Dinas Pendidikan dan Kebudayaan Kota Malang. Tujuan dari kegiatan tersebut adalah mengedukasi/memberikan pengarahan kepada K3S SD se Kota Malang terkait kepangkatan.</h6>
-                    <br>
-                    <img src="assets/img/galeri/galeri9.png" alt="">
-                    <br>
-                    <br>
-                    <h6>Acara ini dihadiri oleh 30 Bapak/Ibu K3S SD se-Kota Malang. Sosialisasi dimulai pukul 13.00 yang dibuka oleh Bapak Sugeng dan sambutan dari Bapak Tujuwarno selaku Kepala Bidang Ketenagaan. Dalam sambutan tersebut Pak Tujuwarno mengatakan â€œKepala sekolah adalah ujung tombak kenaikan pangkat anak buahnya(guru)â€. Maksud dari perkataan tersebut adalah dinaikkan/tidaknya pangkat seorang guru tergantung kepada kepala sekolah di sekolah tersebut.</h6>
-                </div>
-                <!-- <div class="modal-footer">
-                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
-                </div> -->
-            </div>
-        </div>
-    </div>
+    {{-- @include('home.pages.modal.index') --}}
     </div>
 @endsection
 
