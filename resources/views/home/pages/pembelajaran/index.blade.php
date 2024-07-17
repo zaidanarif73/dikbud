de00675c88d8e928d460f8cfc39ab49c7c2c5f3e
24lizikri <muhammadlizikriarromadhon@gmail.com>
2024-07-17 15:42:25 +0700

    pembelajaran

--- resources/views/home/pages/pembelajaran/index.blade.php
+++ resources/views/home/pages/pembelajaran/index.blade.php
@@ -1,5 +1,28 @@
 @extends('home.layouts.master')
-
+    @section("css")
+        <link  href="{{URL::to('/')}}/assets/css/pembelajaran/style.css" rel="stylesheet">
+    @endsection
 @section("content")
-    <p>isi content pembelajaran disini!</p>
+    <div class="container">
+        <header>
+            <br>
+            <h1>LAMAN PEMBELAJARAN</h1>
+        </header>
+        <main>
+            <div class="d-flex justify-content-center" >
+                <div class="pembelajaran" >
+                    <div class="pembelajaran-item">
+                        <a href="{{ route('home.pembelajaran.showsd') }}">
+                        <img src="assets/img/pembelajaran/1.png" alt="">
+                        <h3>Sekolah Dasar</h3>
+                    </div>
+                    <div class="pembelajaran-item">
+                        <a href="{{ route('home.pembelajaran.showsmp') }}">
+                        <img src="assets/img/pembelajaran/2.png" alt="">
+                        <h3>Sekolah Menengah Pertama</h3>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 @endsection
