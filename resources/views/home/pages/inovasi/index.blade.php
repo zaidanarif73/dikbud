4a75e55f3dc652a99d00e7f46ad451dd5469a456
yusuffirmansyah <yusuffirmansyah001@gmail.com>
2024-07-25 09:31:36 +0700

    edit home & inovasi

--- resources/views/home/pages/inovasi/index.blade.php
+++ resources/views/home/pages/inovasi/index.blade.php
@@ -7,6 +7,42 @@
       <img src="{{URL::to('/')}}/assets/img/banner-inov.png" alt="">
     </div>
 <div class="container mt-5">
+    <div class="row">
+        <div class="btn-group col-2">
+            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Tahun</button>
+            <ul id="tahunList" class="dropdown-menu" style="max-height: 200px; overflow-y: scroll;">
+                <li><a class="dropdown-item" href="#">2019</a></li>
+                <li><a class="dropdown-item" href="#">2020</a></li>
+                <li><a class="dropdown-item" href="#">2021</a></li>
+                <li><a class="dropdown-item" href="#">2022</a></li>
+                <li><a class="dropdown-item" href="#">2023</a></li>
+                <li><a class="dropdown-item" href="#">2024</a></li>
+                <li><a class="dropdown-item" href="#">2025</a></li>
+                <li><a class="dropdown-item" href="#">2026</a></li>
+                <li><a class="dropdown-item" href="#">2027</a></li>
+                <li><a class="dropdown-item" href="#">2028</a></li>
+                <li><a class="dropdown-item" href="#">2029</a></li>
+                <li><a class="dropdown-item" href="#">2030</a></li>
+                <li><a class="dropdown-item" href="#">2019</a></li>
+                <li><a class="dropdown-item" href="#">2020</a></li>
+                <li><a class="dropdown-item" href="#">2021</a></li>
+                <li><a class="dropdown-item" href="#">2022</a></li>
+                <li><a class="dropdown-item" href="#">2023</a></li>
+                <li><a class="dropdown-item" href="#">2024</a></li>
+                <li><a class="dropdown-item" href="#">2025</a></li>
+                <li><a class="dropdown-item" href="#">2026</a></li>
+                <li><a class="dropdown-item" href="#">2027</a></li>
+                <li><a class="dropdown-item" href="#">2028</a></li>
+                <li><a class="dropdown-item" href="#">2029</a></li>
+                <li><a class="dropdown-item" href="#">2030</a></li>
+            </ul>
+        </div>
+        <div class="box col-10 d-flex justify-content-end">
+            <input type="text" placeholder="search...">
+            <a href="#"></a>
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
     <main>
         <div class="gallery">
             <div class="gallery-item">
@@ -50,4 +86,23 @@
         <button>&raquo;</button>
     </div>
 </div>
+<form id="frmTahun" method="get">
+    @csrf
+    <input type="hidden" name="year"/>
+</form>
+@endsection
+@section("script")
+    <script>
+        $(document).ready(function () {
+            $('#tahunList li a').on('click', function () {
+                var year= ($(this).text());
+                //alert("Your Favourite Sports is "+txt);
+                $("#frmTahun").attr("action", "{{ route('home.inovasi.index') }}");
+                $("#frmTahun").find('input[name="year"]').val(year);
+                $("#frmTahun").submit();
+                //$("#tahun").val() = txt;
+            });
+        });
+    </script>
+
 @endsection
