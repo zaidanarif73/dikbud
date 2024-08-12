d2aab49071efd27f0908eb872c8cf90c1c63f5f1
Dhevajaya <dhevajaya123@gmail.com>
2024-08-12 09:01:09 +0700

    oko

--- resources/views/home/component/kalender.blade.php
+++ resources/views/home/component/kalender.blade.php
@@ -1,7 +1,7 @@
 <div id="my_kalender" style="padding:10px; background-image: url('assets/img/bgcal.png'); background-attachment:fixed;">
-  <div class="container p-5">
-    <div class="kalender m-5">
-        <div id="calendars"></div>
+    <div class="container p-5">
+        <div class="kalender m-5">
+            <div id="calendars"></div>
+        </div>
     </div>
-  </div>
 </div>
