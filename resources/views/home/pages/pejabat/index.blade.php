380a4c3968be94dcc322fd8562a48dc71e992142
addindarizki <adindakartikasari91@gmail.com>
2024-08-15 15:47:35 +0700

    ketua & pejabat

--- resources/views/home/pages/pejabat/index.blade.php
+++ resources/views/home/pages/pejabat/index.blade.php
@@ -8,27 +8,116 @@
 </section>
 
 {{-- team --}}
-<section id="team" class="team section mt-5">
+<section>
   <!-- Section Title -->
   <div class="container section-title" data-aos="fade-up" style="padding-top: 60px;">
     <h2>KEPALA DINAS DAN PEJABAT</h2>
   </div><!-- End Section Title -->
   
-  <div class="container">
-    <div class="row gy-4 d-flex flex-nowrap overflow-auto justify-content-start">
-      @forelse ($table as $index => $row)
-        <div class="col-xl-3 col-md-6 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-          <div class="member">
-            <img src="{{ asset('storage/' . $row->image) }}" class="img-fluid" alt="">
-            <h4>{{ $row->nama }}</h4>
-            <span>{{ $row->jabatan }}</span>
-          </div>
-        </div><!-- End Team Member -->
-      @empty
-        <p>Tidak ada data</p>
-      @endforelse
+  <section id="portfolio" class="portfolio section">
+
+    <div class="container">
+
+      <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+
+        <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-app">
+            <div class="portfolio-content ">
+              <a href="assets/img/su.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/su.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info ">
+                <h4><a href="portfolio-details.html" title="More Details">Suwarjana S.E m pakahs shmdyh</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-product">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/product-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-1.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Product 1</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-branding">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/branding-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-1.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Branding 1</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-books">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/books-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-1.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Books 1</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-app">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/app-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-2.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">App 2</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-product">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/product-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-2.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Product 2</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-branding">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/branding-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-2.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Branding 2</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-books">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/books-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-2.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">Books 2</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+          <div class="col-lg-3 col-md-3 portfolio-item isotope-item filter-app">
+            <div class="portfolio-content">
+              <a href="assets/img/portfolio/app-3.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-3.jpg" class="img-fluid" alt=""></a>
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html" title="More Details">App 3</a></h4>
+                <p>Lorem ipsum, dolor sit amet consectetur</p>
+              </div>
+            </div>
+          </div><!-- End Portfolio Item -->
+
+        </div><!-- End Portfolio Container -->
+
+      </div>
+
     </div>
-  </div>
-</section>
+
+  </section>
   
 @endsection
